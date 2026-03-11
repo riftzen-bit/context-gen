6b2dba8f5607d803fa73b68000237b114a81b158
riftzen-bit <binb53339@gmail.com>
2026-03-11 16:42:15 -0700

    feat: add MIT license and bump version to v0.2.0

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 Paul. Licensed under the MIT License.
+// See LICENSE file in the project root for full license text.
+
 package main
 
 import "github.com/paul/context-gen/cmd"
